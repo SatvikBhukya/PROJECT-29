1821cf370434195e6713986f75220a5bddb398ea
SatvikBhukya <73173978+SatvikBhukya@users.noreply.github.com>
2021-02-24 08:05:09 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,40 @@
+class Polygon  {
+    constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		this.image = loadImage("polygon.png")
+        
+        World.add(world, this.body);
+        
+
+    }
+    display()  {
+         var circle1pos=this.body.position;		
+    
+                push()
+                translate(circle1pos.x, circle1pos.y);
+                rectMode(CENTER)
+                strokeWeight(3);
+                fill(255,0,255)
+                ellipse(0,0,this.r, this.r);
+                pop()
+                 
+                imageMode(CENTER)
+        image(this.image,this.body.position.x,this.body.position.y,40,40)
+             
+              
+    
+    }
+}
+
